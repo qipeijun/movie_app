28ce4fdab9bfa73414425b3c690e0f560d9062e1
戚培俊 <qipeijun@aliyun.com>
2019-07-17 11:09:13 +0800

    拍照页面

--- lib/photo_page.dart
+++ lib/photo_page.dart
@@ -13,6 +13,8 @@ class PhotoPage extends StatefulWidget{
 
 class _PhotoPage extends State<PhotoPage>{
 
+  List<File> _images = [];
+
   File _image;
 
   Future getImage(isTakePhoto) async {
@@ -20,7 +22,7 @@ class _PhotoPage extends State<PhotoPage>{
     var image = await ImagePicker.pickImage(source:isTakePhoto? ImageSource.camera : ImageSource.gallery);
 
     setState(() {
-      _image = image;
+      _images.add(image);
     });
   }
 
@@ -30,10 +32,10 @@ class _PhotoPage extends State<PhotoPage>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      title: '拍照APP',
+      title: '拍照',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('拍照APP'),
+          title: Text('拍照'),
           leading: GestureDetector(
             onTap: (){
               Navigator.pop(context);
@@ -42,9 +44,11 @@ class _PhotoPage extends State<PhotoPage>{
           ),
         ),
         body: Center(
-          child: _image == null
-              ? Text('No image selected.')
-              : Image.file(_image),
+          child: Wrap(
+            spacing: 5,
+            runSpacing: 5,
+            children: _getImages(),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: _pickImage,
@@ -55,6 +59,7 @@ class _PhotoPage extends State<PhotoPage>{
     );
   }
 
+  /// 下拉菜单 弹框
   void _pickImage() {
     showModalBottomSheet(context: context, builder: (context)=>Container(
       height: 160,
@@ -67,6 +72,7 @@ class _PhotoPage extends State<PhotoPage>{
     ));
   }
 
+  /// 下拉菜单 item
   _item(String title, bool isTakePhoto) {
     return GestureDetector(
       child: ListTile(
@@ -75,5 +81,39 @@ class _PhotoPage extends State<PhotoPage>{
         onTap: ()=> getImage(isTakePhoto),
       ),
     );
+  }
+
+  /// 图片列表
+  _getImages() {
+    return _images.map((item){
+      return Stack(
+        children: <Widget>[
+          ClipRRect(
+            // 圆角效果
+            borderRadius: BorderRadius.circular(5),
+            child: Image.file(item,width: 120,height: 100,fit: BoxFit.fill,),
+          ),
+          Positioned(
+          right: 5,
+          top: 5,
+          child: GestureDetector(
+            onTap: (){
+              setState(() {
+                _images.remove(item);
+              });
+            },
+            child: ClipOval(
+              // 圆角删除按钮
+              child: Container(
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(color: Colors.black54),
+                child: Icon(Icons.close,size: 18,color: Colors.white,),
+              ),
+            ),
+          )
+          ),
+        ],
+    );
+    }).toList();
   }
 }
